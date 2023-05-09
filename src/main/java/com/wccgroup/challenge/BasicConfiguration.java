e911f87d3cf46c70f2a3ef067e0bdbb19cbeb895
Abdul Aziz Ali <abdulazizali@acm.org>
2023-05-09 20:16:59 +0800

    #8 desperate bad impl of basic auth

---
+++ src/main/java/com/wccgroup/challenge/BasicConfiguration.java
@@ -0,0 +1,44 @@
+package com.wccgroup.challenge;
+//https://www.baeldung.com/spring-security-basic-authentication
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user1")
+                .password(passwordEncoder().encode("user1Pass"))
+                .authorities("ROLE_USER");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/securityNone")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
+
+}
