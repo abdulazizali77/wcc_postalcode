7409046a407da1e3ffa218f16ec41c479f2f072e
Abdul Aziz Ali <abdulazizali@acm.org>
2023-05-09 20:14:58 +0800

    fixed prop access

--- src/test/java/com/wccgroup/challenge/PostcodeServiceTests.java
+++ src/test/java/com/wccgroup/challenge/PostcodeServiceTests.java
@@ -20,8 +20,8 @@ public class PostcodeServiceTests {
     void getSuccess() {
         //'AB16 7NX', '57.168438','-2.161636'
         Postcode pc = postcodeService.getPostcode("AB16 7NX");
-        assert (pc.coordinate.latitude == 57.168438);
-        assert (pc.coordinate.longitude == -2.161636);
+        assert (pc.coordinate.getLatitude() == 57.168438);
+        assert (pc.coordinate.getLongitude() == -2.161636);
     }
 
     @Test
@@ -34,8 +34,8 @@ public class PostcodeServiceTests {
     void testDistanceSuccess() {
         Postcode postcode1 = postcodeService.getPostcode("AB16 6SZ");
         Postcode postcode2 = postcodeService.getPostcode("AB21 0AL");
-        Double d = distanceService.calculateDistance(postcode1.coordinate.latitude, postcode1.coordinate.longitude,
-                postcode2.coordinate.latitude, postcode2.coordinate.longitude);
+        Double d = distanceService.calculateDistance(postcode1.coordinate.getLatitude(), postcode1.coordinate.getLongitude(),
+                postcode2.coordinate.getLatitude(), postcode2.coordinate.getLongitude());
 
         //FIXME: yOU sAy juMp! I say how much??
         assert(d == 11.62191361776278);
